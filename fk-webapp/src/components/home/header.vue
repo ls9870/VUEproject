ffe122f6f874e0116d4bf33ceba1d47d90db811c
ls9870 <1571668658@qq.com>
2019-11-04 22:54:10 +0800

    Initial commit

---
+++ fk-webapp/src/components/home/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box-search">
+    <div class="search">
+      <input type="text" name="search" id="search" value="熊本熊"/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "header"
+}
+</script>
+
+<style scoped>
+  .box-search{
+    width: 100%;
+    height: 0.45rem;
+    background:gradient(rgba(209,207,207,1),rgba(209,207,207,0.2));
+  }
+
+  /*.box-search{*/
+  /*  background-color:gradient(rgba(209,207,207,1),rgba(209,207,207,0.2));*/
+  /*}*/
+</style>
---
+++ fk-webapp/src/view/home.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+
+<script>
+import Header from '../components/home/header.vue'
+
+export default {
+  name: "home",
+  components:{
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
